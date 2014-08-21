970de14a0e6d6f991b9dfd4315acc2bfad398d37
Giovanni Bryden <giovanni.bryden@moovweb.com>
2014-08-21 15:43:54 -0700

    More crap. Next step: Figure out how to not make API take so long

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
     require 'net/https'
     require 'json'
 
-    uri = URI('https://api.relateiq.com/v2/lists/53d2cd5ce4b04e37e550208a/listitems/')
+    uri = URI('https://api.relateiq.com/v2/lists/53d2cd5ce4b04e37e550208a/listitems?_start=0&_limit=5')
 
     req = Net::HTTP::Get.new(uri)
     req.basic_auth '53dc0b49e4b088619bfe48f6', 'g9Bb7j6XXSZYOJh5cpngznvUPtl'
